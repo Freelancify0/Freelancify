501de095fe49a8b295eb58517aa00567529cb296
Freelancify0 <freelancify0@gmail.com>
2025-05-03 01:02:00 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -105,7 +105,7 @@ app.use((req, res, next) => {
 });
 // app.use("/api/github", githubOauthRoute);
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://freelancify-swart.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
